9c32ba0ef4274a3d1ea5908199a320277eb79f4e
Anna <annaskorp@gmail.com>
2026-02-12 15:34:19 +0300

    commit 2

--- lesson_3/mailing.py
+++ lesson_3/mailing.py
@@ -1,15 +1,13 @@
-from address import Address
-
 class Mailing:
 
     def __init__(self, track, to_address, from_address, cost):
         self.track = track
-        self.to_address = Address
-        self.from_address = Address
+        self.to_address = to_address
+        self.from_address = from_address
         self.cost = cost
 
     def __str__(self):
         return (f" Отправление {self.track} из {self.from_address}"
-                f"в {self.to_address}. Стоимость {self.cost} рублей.")
+                f" в {self.to_address}. Стоимость {self.cost} рублей.")
 
 
